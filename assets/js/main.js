f2d024fdbaffa5fc3adfcf7d8312fb9b478c519a
Sciuridae0603 <sciuridae0603@gmail.com>
2018-07-13 07:43:44 +0800

    fix footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,17 @@
 if((window.innerWidth > document.documentElement.clientWidth) === false){
-    document.getElementsByClassName("footer")[0].className = document.getElementsByClassName("footer")[0].className + " fixed-bottom";
+    document.getElementById("footer").className = document.getElementById("footer").className + " fixed-bottom";
 }
+
+function set_footer(){
+    var useableContentHeight = document.documentElement.clientHeight - ($("#footer").height() + $("#navbar").height());
+    var currentContentHeight = $("#content").height();
+    console.log(useableContentHeight + "  " + currentContentHeight);
+    if(useableContentHeight >= currentContentHeight){
+        document.getElementById("footer").className = "footer bg-dark p-4 mt-5 fixed-bottom";
+    }else{
+        document.getElementById("footer").className = "footer bg-dark p-4 mt-5";
+    }
+}
+
+window.onload = set_footer();
+document.body.addEventListener("resize",set_footer);
