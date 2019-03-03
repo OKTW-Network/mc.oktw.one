c0c1afc4f3813bf486a66e2f0547649a8b5de223
Sciuridae0603 <Sciuridae0603@gmail.com>
2019-03-03 23:14:17 +0800

    Fix deploy script build on gh-pages again

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -9,16 +9,18 @@ mv ./../.git ./../_site ./
 
 git checkout gh-pages
 
-mv ./../.circleci ./../CNAME ./
 
 find . -maxdepth 1 ! -name '.git' ! -name '_site' ! -name '.circleci' ! -name 'CNAME' -exec rm -rf {} \;
 
 mv _site/* ./
+mv ./../.circleci ./../CNAME ./
 
+rm ./_site -rf
 rm ./Dockerfile -rf
 rm ./now.json -rf
 
 git add .
+git add .circleci
 git commit --allow-empty -m "$(git log master -1 --pretty=%B)"
 git push --set-upstream origin gh-pages
 
