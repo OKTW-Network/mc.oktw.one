03c67366ae15aa56bb4d1bbf0b6d5cd46468f6a3
Sciuridae0603 <Sciuridae0603@gmail.com>
2019-03-03 21:37:57 +0800

    Fix deploy script again

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -8,7 +8,7 @@ mv .circleci ./../
 rm -rf ./*
 mv ./../.git ./
 
-git checkout gh-pages --track origin/gh-pages
+git checkout gh-pages
 
 find . -maxdepth 1 ! -name '_site' ! -name '.git' ! -name '.gitignore' ! -name '.circleci' -exec rm -rf {} \;
 
