44c921e8e7873a546d631982568425d66a8f7701
Sciuridae0603 <Sciuridae0603@gmail.com>
2019-03-03 22:15:57 +0800

    Add CircleCI deploy

---
+++ .circleci/deploy.sh
@@ -0,0 +1,25 @@
+git config user.name "CircleCI"
+git config user.email "circleci@oktw.network"
+
+mv _site ./../
+mv .git ./../
+mv .circleci ./../
+
+rm -rf ./*
+mv ./../.git ./
+
+git checkout gh-pages
+
+find . -maxdepth 1 ! -name '_site' ! -name '.git' ! -name '.gitignore' ! -name '.circleci' -exec rm -rf {} \;
+
+mv ./../_site/* ./
+
+rm ./Dockerfile -rf
+rm ./now.json -rf
+
+git add .
+git add .circleci
+git commit --allow-empty -m "$(git log master -1 --pretty=%B)"
+git push --set-upstream origin gh-pages
+
+echo "Deployed successfully"
