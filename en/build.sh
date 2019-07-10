2d8d151cbf4585e9fd683c2cc954731363ca2d26
CircleCI <circleci@oktw.network>
2019-07-10 11:44:02 +0000

    Fix build.sh

--- en/build.sh
+++ en/build.sh
@@ -1,7 +1,11 @@
-yum install ruby rubygems
-
+yum install gcc-c++ patch readline readline-devel zlib zlib-devel libyaml-devel libffi-devel openssl-devel make bzip2 autoconf automake libtool bison iconv-devel sqlite-devel
+curl -sSL https://rvm.io/mpapis.asc | gpg --import -
+curl -L get.rvm.io | bash -s stable
+source /etc/profile.d/rvm.sh
+rvm reload
+rvm requirements run
+rvm install 2.6.3
+rvm use 2.6.3 --default
 gem install jekyll bundler
-
 bundle install
-
 jekyll build
