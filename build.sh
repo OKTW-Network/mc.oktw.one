2f8666e5fa4e6a42fec4a20580666fcb3c2e7ec9
CircleCI <circleci@oktw.network>
2019-07-10 11:59:21 +0000

    Upgrade Now CI to Version 2

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 yum install gcc-c++ patch readline readline-devel zlib zlib-devel libyaml-devel libffi-devel openssl-devel make bzip2 autoconf automake libtool bison iconv-devel sqlite-devel
-curl -sSL https://rvm.io/mpapis.asc | gpg --import -
-curl -L get.rvm.io | bash -s stable
+wget -O rvm.asc https://rvm.io/mpapis.asc
+gpg --import rvm.asc
+wget -O rvm.sh get.rvm.io
+chmod +x rvm.sh
+./rvm.sh -s stable
 source /etc/profile.d/rvm.sh
 rvm reload
 rvm requirements run
