9175877218d02c449270d98176b0cc65c6865ae6
CircleCI <circleci@oktw.network>
2019-07-11 11:41:49 +0000

    Fix CRLF

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
-yum install gcc-c++ patch readline readline-devel zlib zlib-devel libyaml-devel libffi-devel openssl-devel make bzip2 autoconf automake libtool bison iconv-devel sqlite-devel
-wget -O rvm.asc https://rvm.io/mpapis.asc
+yum install wget gcc-c++ patch readline readline-devel zlib zlib-devel libyaml-devel libffi-devel openssl-devel make bzip2 autoconf automake libtool bison iconv-devel sqlite-devel
+wget -O rvm.asc "https://rvm.io/mpapis.asc"
 gpg --import rvm.asc
-wget -O rvm.sh get.rvm.io
+wget -O rvm.sh "get.rvm.io"
 chmod +x rvm.sh
 ./rvm.sh -s stable
 source /etc/profile.d/rvm.sh
