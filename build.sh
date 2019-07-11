c0d55ebc8ebb9d232fbb5bc92c9cd3cad02648d9
CircleCI <circleci@oktw.network>
2019-07-11 11:49:11 +0000

    Fix RVM install

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 yum install wget gcc-c++ patch readline readline-devel zlib zlib-devel libyaml-devel libffi-devel openssl-devel make bzip2 autoconf automake libtool bison iconv-devel sqlite-devel
 wget -O rvm.asc "https://rvm.io/mpapis.asc"
 gpg --import rvm.asc
-wget -O rvm.sh "get.rvm.io"
-chmod +x rvm.sh
-bash ./rvm.sh -s stable --ruby
+wget -O rvm-installer "get.rvm.io"
+chmod +x rvm-installer
+bash ./rvm-installer
 source /etc/profile.d/rvm.sh
 rvm reload
 rvm requirements run
