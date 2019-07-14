80408d881089c6c1b336488612c42d0ce302531f
CircleCI <circleci@oktw.network>
2019-07-14 12:07:43 +0000

    Debug

--- build.sh
+++ build.sh
@@ -1,18 +1,29 @@
-export PATH=$PATH:/usr/sbin/:/usr/local/rvm/bin/
-yum install wget gcc-c++ patch readline readline-devel zlib zlib-devel libyaml-devel libffi-devel openssl-devel make bzip2 autoconf automake libtool bison iconv-devel sqlite-devel
+export PATH=$PATH:/usr/sbin/:/usr/local/rvm/bin/:/usr/local/rvm/src/ruby-2.6.3/bin/
+
+# Remove original Ruby
+yum remove ruby -y
+
+# Install RVM Public GPG Key
 wget -O rvm.asc "https://rvm.io/mpapis.asc"
 gpg --import rvm.asc
+
+# Install RVM
 wget -O rvm-installer "get.rvm.io"
 chmod +x rvm-installer
-bash ./rvm-installer
+bash ./rvm-installer 
+
+# Install Ruby 2.6.3
 source /etc/profile.d/rvm.sh
 rvm reload
 rvm requirements run
 rvm install 2.6.3
 rvm use 2.6.3 --default
-/usr/local/rvm/src/ruby-2.6.3/bin/gem install jekyll bundler
-/usr/local/rvm/src/ruby-2.6.3/bin/bundler install
-/usr/local/rvm/src/ruby-2.6.3/bin/jekyll build
+
+# Install Jekyll
+gem install jekyll bundler
+bundler install
+jekyll build
+
 find / -iname "rvm"
 find / -iname "ruby"
 find / -iname "gem"
